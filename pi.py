1860d663045481356d13683bb64a916dbae3f0a0
Kacper Latuszewski <kacpx123@gmail.com>
2019-10-19 00:05:24 +0200

    index.html

--- pi.py
+++ pi.py
@@ -8,10 +8,7 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-while True:
-    webbrowser.open_new_tab('zastepstwa.html')
-    time.sleep(5) #open html
-    keyboard.press(Key.f11)
-    keyboard.release(Key.f11)
+while True
+    keyboard.press('f5')
+	keyboard.release('f5')
     time.sleep(14400) #every 4h
-    os.system("killall -KILL chromium-browse") #close chrome
--- push_file.py
+++ push_file.py
@@ -3,7 +3,9 @@
 import pysftp as sftp
 
 def push_file_to_server():
-    f = sftp.Connection(host='192.168.43.222', username='pi', password='qwerty1231')
+    cnopts = sftp.CnOpts()
+    cnopts.hostkeys = None 
+    f = sftp.Connection(host='192.168.43.222', username='pi', password='qwerty1231', cnopts=cnopts)
     local_path = "zastepstwa.html"
     remote_path = "/home/pi/Desktop/zastepstwa-projekt/zastepstwa.html"
 
