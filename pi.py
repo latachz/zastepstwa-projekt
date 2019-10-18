0e64a1b1b692ce9b03ba5ed28a3901bc07ed2003
Kacper Latuszewski <16508923+latachz@users.noreply.github.com>
2019-10-19 00:07:45 +0200

    Update pi.py

--- pi.py
+++ pi.py
@@ -8,15 +8,9 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-<<<<<<< HEAD
-while True
-    keyboard.press('f5')
-	keyboard.release('f5')
-    time.sleep(14400) #every 4h
-=======
 while True:
     webbrowser.open_new_tab('zastepstwa.html')
     keyboard.press(Key.f5)
     keyboard.release(Key.f5)
     time.sleep(14400) #every 4h
->>>>>>> b8350f846c4248e989f931c8bef0eaf05d207202
+
