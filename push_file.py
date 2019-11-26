79ce4a3043feecfa1a761d2d8bc31cf85982da13
Kacper Latuszewski <kacpx123@gmail.com>
2019-11-26 13:05:08 +0100

    comp

--- push_file.py
+++ push_file.py
@@ -4,8 +4,8 @@ import pysftp as sftp
 
 def push_file_to_server():
     f = sftp.Connection(host='192.168.220.38', username='pi', password='qwerty1231')
-    local_path = "zastepstwa.html"
-    remote_path = "/home/pi/Desktop/zastepstwa-gui/zastepstwa.html"
+    local_path = "Zastępstwa.html"
+    remote_path = "/home/pi/Desktop/zastepstwa-gui/Zastępstwa.html"
 
     f.put(local_path, remote_path)
     f.close()
