b622daa3dc8ddb31486f39abe23d154a573fe5bc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-10 15:05:14 +0100

    Add private key

--- push_file.py
+++ push_file.py
@@ -3,7 +3,9 @@
 import pysftp as sftp
 
 def push_file_to_server():
-    f = sftp.Connection(host='192.168.220.38', username='pi', password='qwerty1231')
+    # `ssh-keygen` already outputs in pem format so we need just to add `.pem` extension
+    private_key_location = 'key.pem'
+    f = sftp.Connection(host='192.168.220.38', username='pi', private_key=private_key_location)
     local_path = "Zastępstwa.html"
     remote_path = "/home/pi/Desktop/zastepstwa-gui/Zastępstwa.html"
 
